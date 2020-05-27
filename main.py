a64f5ab906093922d21ee1024b98b1c9f50099b0
Nikita Gorodilov <healyou1994@gmail.com>
2020-05-27 17:50:34 +0300

    Выполнение нескольких задач в цикле

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 from selenium.webdriver import Chrome
 from command.creator.factory import *
 from command.commands import FactoryCommand
-from utils.util import get_absolute_file_path
+from utils.util import *
 
 browser = Chrome(executable_path='install/chromedriver.exe')
 try:
     browser.get('https://ts3.travian.ru')
-    file_path = get_absolute_file_path(__file__, 'files/open_travian.json')
-    creator = JsonCommandCreator(browser, file_path)
-    command = creator.create_command()
-    command.execute()
+
+    for path in get_travian_command_files():
+        file_path = get_absolute_file_path(__file__, path)
+        creator = JsonCommandCreator(browser, file_path)
+        command = creator.create_command()
+        command.execute()
+
 except OSError as err:
     print('Ошибка')
 finally:
--- utils/util.py
+++ utils/util.py
@@ -3,3 +3,10 @@ import os
 def get_absolute_file_path(cur_script_file, rel_file_path):
     script_dir = os.path.dirname(cur_script_file)
     return os.path.join(script_dir, rel_file_path)
+
+def get_travian_command_files():
+    return [
+        'files/travian/login.json',
+        'files/travian/open_village.json',
+        'files/travian/open_map.json'
+    ]
